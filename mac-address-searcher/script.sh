fd211db04b13fa65476e8569d7c072e216ff3789
Arne De Herdt <arne.de.herdt@gmail.com>
2023-12-17 19:02:15 +0100

    Fixed script

--- mac-address-searcher/script.sh
+++ mac-address-searcher/script.sh
@@ -7,6 +7,7 @@
 # Ask where the log files are located.
 echo "Please enter the full path to the directory where the log files are located:"
 read -r log_directory
+files_to_search=$(find "${log_directory}" -regextype posix-extended -regex '.*(\b[1-9]\b|\b[12][0-9]\b|30)')
 
 # First step is reading the contents of the file "mac-addresses.txt" and storing it in an array.
 # An array allows us to just hold the data as a single collection so we can loop through it afterwards.
@@ -17,22 +18,38 @@ readarray -t lines < mac-addresses.txt
 echo "Loaded ${#lines[@]} mac addresses to search for."
 
 # Loop through each mac address in the array, and search for the entry in the files located in the directory.
-for mac_address in "${lines[@]}"
+for key in "${!lines[@]}"
 do
+  echo "Processing ${key} of ${#lines[@]}"
+
   # Remove special characters from Windows such as the carriage return.
   # Otherwise the search will fail as the input gets messed up.
+  mac_address=${lines[$key]}
   cleaned_entry=${mac_address//$'\r'}
+  entry_found=false
 
-  # Search for the mac address in the log files using find to select all the files we want
-  # and then grep to search for the mac address in those files.
-  search_result=$(find "${log_directory}" -regextype posix-extended -regex '.*(\b[1-9]\b|\b[12][0-9]\b|30)' -exec grep "$cleaned_entry" {} + | wc -l)
+  # Loop through each file we have selected from the provided directory.
+  for file in $files_to_search
+  do
+    # Use grep to search for our clean mac-address, and return on the first match.
+    # Match returns 1, otherwise we will get 0
+    search_result=$(grep -m1 -i -c "$cleaned_entry" "$file")
 
-  # If the search result is greater than 0, then we found the mac address in the log files.
-  # If not, then we echo the mac address to the user.
-  if [ "$search_result" -gt 0 ];
+    # If we found a match, break the loop and set our flag so we don't print out the mac-address.
+    # otherwise, proceed with the loop to the next file.
+    if [ "${search_result}" -gt 0 ];
+    then
+      entry_found=true
+      break
+    else
+      true
+    fi
+  done
+
+  # If we looped over all entries (or aborted the inner loop cause we found a hit),
+  # we need to check if managed to find the entry and print it out or not.
+  if [ "${entry_found}" = false ];
   then
-    true
-  else
-    echo "$cleaned_entry"
+    echo "${cleaned_entry}" >> "output.txt"
   fi
 done
